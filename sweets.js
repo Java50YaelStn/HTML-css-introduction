b2603844a883b8cfe6298c9f5714d45181b172f2
yael.stn <stolshtein2022@mail.ru>
2022-12-15 21:23:27 +0200

    add HW09

--- sweets.js
+++ sweets.js
@@ -1,14 +1,5 @@
-// alert("NEW YEAR'S SWEETS");
-// let a = +prompt("hahahaha", 5);
-// if (a==10) {
-// alert("a=10");
-// }
-// else{ alert("a!=10");}
-const detailsImage = document.querySelector(".details-image");//moje bit let. eto kartinka
-// console.log(detailsImage);
-// let a = document.getElementById("NEWYEAR");
-// alert(a);
-// a.style.width="1000px";
+
+const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
 const thumbnailsTitle = document.querySelectorAll(".thumbnails-title");
@@ -21,20 +12,12 @@ for (let index = 0; index < b.length; index++) {
     
 }
 
-// for (let index = 0; index < thumbnailsTitle.length; index++) {
-//     if (index%2 ==0) {
-// console.log (thumbnailsTitle[index]);
-// thumbnailsTitle[index].style.cssText="background:white;border:5px solid black";
-//     }
-//     else 
-//     thumbnailsTitle[index].style.background= "red";  
-// }
+
 function setDetails(anchor) {
     let dataImage = anchor.getAttribute("data-details-image");
     console.log (dataImage);
     detailsImage.src = dataImage;
-    detailsTitle.innerHTML = anchor.getAttribute("data-details-title");//menaet soderjimoe podpis
-
+    detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
 }
 for (let i = 0; i < anchorElements.length; i++) {
     anchorElements[i].addEventListener("click",function(){
